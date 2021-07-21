df9fe280c5d52672cd58a3dc9b20a46117ce297e
keith <keithontiveros@ucla.edu>
2021-07-21 14:24:28 -0700

    Update decks.py with new functions

--- flashcards/deck.py
+++ flashcards/deck.py
@@ -1,3 +1,4 @@
+import os
 import csv
 import random
 from os import path, remove
@@ -14,20 +15,24 @@ class Card:
 class Deck:
     """Stores a list of flashcards."""
 
-    def __init__(self, filename):
+    def __init__(self, deckpath):
         """Loads a deck from an existing file"""
-        self.filename = filename
+        self.deck_path = deckpath
         self.cards = []
-        with open(filename) as csvfile:
-            reader = csv.reader(csvfile, delimiter='\t')
-            next(reader)
-            for row in reader:
-                self.cards.append(Card(row[0], row[1]))
+        if path.exists(self.deck_path):
+            with open(self.deck_path) as csvfile:
+                reader = csv.reader(csvfile, delimiter='\t')
+                next(reader)
+                for row in reader:
+                    self.cards.append(Card(row[0], row[1]))
+        else:
+            with open(self.deck_path):
+                pass
 
     def __del__(self):
         """Removes deck csv file"""
-        if path.exists('decks/' + self.filename):
-            remove('decks/' + self.filename)
+        if path.exists(self.deck_path):
+            remove(self.deck_path)
 
     def __iter__(self):
         return DeckIterator(self.cards)
@@ -44,7 +49,7 @@ class Deck:
 
     def save(self):
         """Save the deck as a csv."""
-        with open(self.filename, 'w') as file:
+        with open(self.deck_path, 'w') as file:
             writer = csv.writer(file, delimiter='\t')
             writer.writerow(['Front', 'Back'])
             for card in self.cards:
@@ -64,3 +69,15 @@ class DeckIterator:
             return self.cards.pop()
         else:
             raise StopIteration
+
+
+def decks_list():
+    """Returns a sorted list of all the decks that are saved"""
+    decks = []
+    deck_dir = os.path.join('flashcards', 'decks')
+    for d in os.listdir(deck_dir):
+        deck_path = os.path.join(deck_dir, d)
+        if os.path.isfile(deck_path):
+            decks.append(d)
+    decks.sort()
+    return decks
